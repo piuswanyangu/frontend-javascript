36d3ecce6c390f029ed1c0afa3ed75b9fdcc9117
pius wanyangu <ndubipius96@gmail.com>
2025-12-03 00:32:51 +0300

    adding updated task_4 main.ts

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -1,10 +1,4 @@
-// create and export a constant cpp for Cpp Subjects
-// create and export a constant java for Java Subjects
-// create and export a constant react for React Subjects
-// create and export one Teacher object cTeacher with experienceTeachingC = 10
-// for Cpp subject, log to the console C++, set cTeacher as the teacher, call the two methods getRequirements and getAvailableTeacher and print the strings they return
-// for Java subject, log to the console Java, set cTeacher as the teacher, call the two methods getRequirements and getAvailableTeacher, and print the strings they return
-// for React subject, log to the console React, set cTeacher as the teacher, call the two methods getRequirements and getAvailableTeacher, and print the strings they return
+
 
 // 1 export constants for eah subject
 export const cpp = new Subjects.Cpp()
