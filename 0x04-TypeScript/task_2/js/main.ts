91877295c3e2cd0fc89c7c48b624ea16810af076
pius wanyangu <ndubipius96@gmail.com>
2025-12-02 22:36:48 +0300

    adding create functions specific to employee

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,30 @@
+// Write a function isDirector:
+// it accepts employee as an argument
+// it will be used as a type predicate and if the employee is a director
+// Write a function executeWork:
+// it accepts employee as an argument
+// if the employee is a Director, it will call workDirectorTasks
+// if the employee is a Teacher, it will call workTeacherTasks
+// /teacher and directors interface
+
+interface Teacher {
+    workTeacherTask(): string;
+}
+
+interface Director {
+    workDirectorTasks(): string;
+}
+
+// check if employee is a director
+function isDirector(employee: Teacher | Director): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+// function to execute the correct work method
+function executeWork(employee: Teacher | Director): string {
+    if (isDirector(employee)){
+        return employee.workDirectorTasks();
+    } else {
+        return employee.workTeacherTask();
+    }
+}
